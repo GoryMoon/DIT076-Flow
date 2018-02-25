38ebf848b982ba2a58f92b29afa81e37fef6479e
magnlar <cmr.larsson@gmail.com>
2018-02-26 00:46:02 +0100

    First server commit. WS API has partial user and post functionality - still under construction

---
+++ server/src/main/java/com/fgm/flow/dao/UserRegistry.java
@@ -0,0 +1,61 @@
+package com.fgm.flow.dao;
+
+import com.fgm.flow.core.User;
+//import static java.lang.System.out;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import com.fgm.flow.core.QUser;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+/**
+ *
+ * @author fgm
+ */
+@Stateless
+public class UserRegistry extends AbstractQuery<User, Integer>
+{
+
+    @PersistenceContext(unitName = "flow_pu")
+    private EntityManager em;
+      
+
+    public UserRegistry()
+    {
+        super(User.class, QUser.user);
+    }
+
+    @Override
+    public EntityManager getEntityManager()
+    {
+        return em;
+    }
+    @Override
+    public void setEntityManager(EntityManager em)
+    {
+        this.em = em;
+    }
+    
+    public List<User> findByNick(String nick)
+    {
+        QUser user = QUser.user;
+        JPAQueryFactory qf = new JPAQueryFactory(em);
+        List<User> found = qf.select(user)
+                .from(user)
+                .where(user.nick.eq(nick))
+                .fetch();
+        return found;
+    }
+    
+    public List<User> findByEmail(String email)
+    {
+        QUser user = QUser.user;
+        JPAQueryFactory qf = new JPAQueryFactory(em);
+        List<User> found = qf.select(user)
+                .from(user)
+                .where(user.email.eq(email))
+                .fetch();
+        return found;
+    }
+}
---
+++ server/target/generated-sources/java/com/fgm/flow/core/QUser.java
@@ -0,0 +1,48 @@
+package com.fgm.flow.core;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QUser is a Querydsl query type for User
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QUser extends EntityPathBase<User> {
+
+    private static final long serialVersionUID = -1251510263L;
+
+    public static final QUser user = new QUser("user");
+
+    public final StringPath email = createString("email");
+
+    public final NumberPath<Integer> id = createNumber("id", Integer.class);
+
+    public final StringPath nick = createString("nick");
+
+    public final StringPath password = createString("password");
+
+    public final ListPath<Post, QPost> posts = this.<Post, QPost>createList("posts", Post.class, QPost.class, PathInits.DIRECT2);
+
+    public final DateTimePath<java.util.Date> time = createDateTime("time", java.util.Date.class);
+
+    public QUser(String variable) {
+        super(User.class, forVariable(variable));
+    }
+
+    public QUser(Path<? extends User> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUser(PathMetadata metadata) {
+        super(User.class, metadata);
+    }
+
+}
+

7b3843171065e07e39ba5648a60a83c69937bbd1
magnlar <cmr.larsson@gmail.com>
2018-02-28 00:31:27 +0100

    Further development of WS API

---
+++ server/src/main/java/com/fgm/flow/dao/CommentRegistry.java
@@ -0,0 +1,50 @@
+package com.fgm.flow.dao;
+
+import com.fgm.flow.core.Post;
+//import static java.lang.System.out;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import com.fgm.flow.core.QPost;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+/**
+ *
+ * @author fgm
+ */
+@Stateless
+public class PostRegistry extends AbstractQuery<Post, Integer>
+{
+
+    @PersistenceContext(unitName = "flow_pu")
+    private EntityManager em;
+      
+
+    public PostRegistry()
+    {
+        super(Post.class, QPost.post);
+    }
+
+    @Override
+    public EntityManager getEntityManager()
+    {
+        return em;
+    }
+    @Override
+    public void setEntityManager(EntityManager em)
+    {
+        this.em = em;
+    }
+    
+    public List<Post> findById(int id)
+    {
+        QPost post = QPost.post;
+        JPAQueryFactory qf = new JPAQueryFactory(em);
+        List<Post> found = qf.select(post)
+                .from(post)
+                .where(post.id.eq(id))
+                .fetch();
+        return found;
+    }
+}
---
+++ server/src/main/java/com/fgm/flow/rest/CommentResource.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fgm.flow.rest;
+
+import com.fgm.flow.core.Post;
+import com.fgm.flow.core.User;
+import com.fgm.flow.dao.PostRegistry;
+import com.fgm.flow.dao.UserRegistry;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.net.URI;
+import java.util.List;
+import static java.lang.System.out;
+import javax.ejb.EJB;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author fgm
+ */
+@Path("post")
+public class PostResource {
+
+    //private static final Logger LOG = Logger.getLogger(UserResource.class.getName());
+
+    @Context
+    private UriInfo uriInfo;
+
+    @EJB
+    private PostRegistry postReg;
+    @EJB
+    private UserRegistry userReg;
+    private final Gson gson = new Gson();
+    
+    GsonBuilder gb = new GsonBuilder();
+    Gson gsonEWE = gb.excludeFieldsWithoutExposeAnnotation().create();
+
+    /*
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    public Response create(Post post)
+    {
+        post.timeStamp();
+        
+        User poster = postReg.find(post.getPoster());
+        product.setUser(user);
+        
+        
+        postReg.create(post);
+
+        // Respond with status 'ok' and only the ID if the
+        // nick did not already exist
+        return Response.ok().build();
+    }
+    */
+    
+    @POST
+    @Path("create")
+    @Consumes({MediaType.APPLICATION_FORM_URLENCODED})
+    public Response create(
+            @FormParam("title") String title,
+            @FormParam("text") String text,
+            @FormParam("usergroup") String userGroup,
+            @FormParam("poster") int posterId
+        ) 
+    {
+        User poster = userReg.find(posterId);
+        
+        if(poster != null)
+        {
+            Post post = new Post(title, text, userGroup, poster);
+
+            postReg.create(post);
+                
+            poster.addPost(post);
+        
+            URI postUri = uriInfo
+                    .getAbsolutePathBuilder()
+                    .path(String.valueOf(post.getId()))
+                    .build(post);
+            
+            return Response.created(postUri).build();
+
+        }
+        
+        return Response.status(403).build();
+        
+    }
+   
+    
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response findAll()
+    {        
+        List<Post> posts = postReg.findAll();
+        
+        out.println(posts);
+        out.println(gsonEWE.toJson(posts));
+        
+        return Response.ok(gsonEWE.toJson(posts)).build();
+    }
+}
